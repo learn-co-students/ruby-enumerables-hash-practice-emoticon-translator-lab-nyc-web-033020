eb14b621bcc423299b0fbe6af81afda3dc87c7f8
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-30 04:44:40 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,37 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(path)
+  library = { get_emoticon: {}, get_meaning: {}}
+  YAML.load_file(path).each_with_object(library) do |(key, val), hash|
+    hash[:get_emoticon][val[0]] = val[1]
+    hash[:get_meaning][val[1]] = key
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
+# returns "☜(⌒▽⌒)☞" for emoticon = "O:)"
+def get_japanese_emoticon(path, emoticon)
+  pair = load_library(path)[:get_emoticon].map do |english, japanese|
+  # pair = load_library(path).map do |word, (english, japanese)|
+    if english == emoticon
+      # return to be "mapped" to pair
+      japanese
+    end
+  # remove nil values from loops that didnt map anything (i.e. failed if check)
+  end.compact
+
+  pair.first ? pair.first : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+# returns "angel" for emoticon = "☜(⌒▽⌒)☞"
+def get_english_meaning(path, emoticon)
+  pair = load_library(path)[:get_meaning].map do |japanese_emoticon, meaning|
+    if japanese_emoticon == emoticon
+      # return to be "mapped" to pair
+      meaning
+    end
+  # remove nil values from loops that didnt map anything (i.e. failed if check)
+  end.compact
+
+  pair.first ? pair.first : "Sorry, that emoticon was not found"
 end
